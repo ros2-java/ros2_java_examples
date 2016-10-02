035135b7a970c1180887443781467ac179ef92f2
Esteve Fernandez <esteve@apache.org>
2016-10-02 17:49:02 +0200

    Fix style

--- rcljava_examples/src/main/java/org/ros2/rcljava/examples/Listener.java
+++ rcljava_examples/src/main/java/org/ros2/rcljava/examples/Listener.java
@@ -12,43 +12,47 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.ros2.rcljava.examples;
 
-import org.ros2.rcljava.RCLJava;
-import org.ros2.rcljava.Node;
-import org.ros2.rcljava.Subscription;
 import org.ros2.rcljava.Consumer;
+import org.ros2.rcljava.Node;
+import org.ros2.rcljava.RCLJava;
+import org.ros2.rcljava.Subscription;
 
 
 public class Listener {
 
-    public static void chatterCallback(std_msgs.msg.String msg) {
-        System.out.println("I heard: " + msg.getData());
-    }
+  public static void chatterCallback(std_msgs.msg.String msg) {
+    System.out.println("I heard: " + msg.getData());
+  }
 
-    public static void main(String[] args) throws InterruptedException {
-        // Initialize RCL
-        RCLJava.rclJavaInit();
+  public static void main(String[] args) throws InterruptedException {
+    // Initialize RCL
+    RCLJava.rclJavaInit();
 
-        // Let's create a new Node
-        Node node = RCLJava.createNode("listener");
+    // Let's create a new Node
+    Node node = RCLJava.createNode("listener");
 
-        // Subscriptions are type safe, so we'll pass the message type. We use the fully qualified
-        // class name to avoid any collision with Java's String class
-        Subscription<std_msgs.msg.String> sub = node.<std_msgs.msg.String>createSubscription(
-            std_msgs.msg.String.class, "chatter", new Consumer<std_msgs.msg.String>() {
+    // Subscriptions are type safe, so we'll pass the message type. We use the
+    // fully qualified class name to avoid any collision with Java's String
+    // class
+    Subscription<std_msgs.msg.String> sub =
+        node.<std_msgs.msg.String>createSubscription(std_msgs.msg.String.class,
+          "chatter", new Consumer<std_msgs.msg.String>() {
 
-            // We define the callback inline, this works with Java 8's lambdas too, but we use
-            // our own Consumer interface because Android supports lambdas via retrolambda, but not
-            // the lambda API
+            // We define the callback inline, this works with Java 8's lambdas
+            // too, but we use our own Consumer interface because Android
+            // supports lambdas via retrolambda, but not the lambda API
             @Override
             public void accept(std_msgs.msg.String msg) {
-                chatterCallback(msg);
+              chatterCallback(msg);
             }
-        });
+          }
+    );
 
-        while(RCLJava.ok()) {
-            RCLJava.spinOnce(node);
-        }
+    while (RCLJava.ok()) {
+      RCLJava.spinOnce(node);
     }
+  }
 }
--- rcljava_examples/src/main/java/org/ros2/rcljava/examples/Talker.java
+++ rcljava_examples/src/main/java/org/ros2/rcljava/examples/Talker.java
@@ -12,39 +12,39 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.ros2.rcljava.examples;
 
-import org.ros2.rcljava.RCLJava;
 import org.ros2.rcljava.Node;
 import org.ros2.rcljava.Publisher;
+import org.ros2.rcljava.RCLJava;
 
-import java.util.Arrays;
-import java.util.ArrayList;
 
 public class Talker {
-    public static void main(String[] args) throws InterruptedException {
-        // Initialize RCL
-        RCLJava.rclJavaInit();
+  public static void main(String[] args) throws InterruptedException {
+    // Initialize RCL
+    RCLJava.rclJavaInit();
 
-        // Let's create a Node
-        Node node = RCLJava.createNode("talker");
+    // Let's create a Node
+    Node node = RCLJava.createNode("talker");
 
-        // Publishers are type safe, make sure to pass the message type
-        Publisher<std_msgs.msg.String> chatter_pub =
-            node.<std_msgs.msg.String>createPublisher(std_msgs.msg.String.class, "chatter");
+    // Publishers are type safe, make sure to pass the message type
+    Publisher<std_msgs.msg.String> chatterPublisher =
+        node.<std_msgs.msg.String>createPublisher(
+        std_msgs.msg.String.class, "chatter");
 
-        std_msgs.msg.String msg = new std_msgs.msg.String();
+    std_msgs.msg.String msg = new std_msgs.msg.String();
 
-        int i = 1;
+    int i = 1;
 
-        while(RCLJava.ok()) {
-            msg.setData("Hello World: " + i);
-            i++;
-            System.out.println("Publishing: \"" + msg.getData() + "\"");
-            chatter_pub.publish(msg);
+    while (RCLJava.ok()) {
+      msg.setData("Hello World: " + i);
+      i++;
+      System.out.println("Publishing: \"" + msg.getData() + "\"");
+      chatterPublisher.publish(msg);
 
-            // Sleep a little bit between each message
-            Thread.sleep(1000);
-        }
+      // Sleep a little bit between each message
+      Thread.sleep(1000);
     }
+  }
 }
